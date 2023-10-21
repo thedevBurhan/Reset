644326bdea11ff94a1f6de899a81df81d4030fef
thedevBurhan <smdburhan25@gmail.com>
2023-10-21 17:26:08 +0530

    error message added

--- Routers/users.js
+++ Routers/users.js
@@ -69,10 +69,10 @@ router.post("/login",async(req,res)=>{
             user.password
         )//compare my hashed and password in req.body
         if(!validPassword){
-            return res.status(400).json({data:"Invalid Password"})
+            return res.status(400).json({data:{message:"Invalid Password",result:validPassword,statusCode:400}})
         }
         
-        res.status(200).json({data:{message:"Sucessfully Logged In",result:validPassword}})
+        res.status(200).json({data:{message:"Sucessfully Logged In",result:validPassword,statusCode:200}})
     } catch (error) {
         console.log(error)
         res.send(500).json({data:"Internal Server Error"})
@@ -104,16 +104,17 @@ router.post('/forgot-password', async (req, res) => {
      
     }
    const token=generateRandomString(20);
-   console.log({token:token} );
+  
 const randomStrings = token;
 user.restToken = randomStrings;
 await user.save();
-console.log(user);
+// console.log(user.email);
   // Send the reset password link to the user's email
   const resetLink = `https://resetpasswordflowmb.netlify.app/ChangePassword/${randomStrings}`; 
 
   // Define the sendResetEmail function
 const sendResetEmail = (email, resetLink) => {
+  
   const transporter = nodemailer.createTransport({
     host:process.env.HOST,
     service:process.env.SERVICE,
